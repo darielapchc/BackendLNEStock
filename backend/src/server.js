bf8e9f00c1fa65cbf1a05e9c486c25d3950d410b
LiaJael <liamaradiaga2007@gmail.com>
2026-09-07 23:33:40 -0600

    refactor: ajustar comentarios y corregir nombre del servidor en server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,21 @@ const sequelize = require('./config/database');
 
 // Importamos los modelos aquí para que Sequelize registre las
 // asociaciones (User.hasMany(RefreshToken), etc.) antes del sync/arranque.
+
+/* Mientras defino bien los modelos
 require('./models/user.model');
 require('./models/refreshToken.model');
 require('./models/categoria.model');
 require('./models/proveedor.model');
+*/
 
 const PORT = process.env.PORT || 4000;
 
 async function startServer() {
   try {
+    //Comprobamos la conexión a MySQL
     await sequelize.authenticate();
+
     console.log('✅ Conexión a MySQL establecida correctamente.');
 
     // ⚠️ IMPORTANTE PARA EL ESTUDIANTE:
@@ -22,13 +27,17 @@ async function startServer() {
     // producción -- puede borrar o alterar datos de forma destructiva.
     // En producción se usan migraciones explícitas (sequelize-cli).
     // Lo veremos formalmente en la Semana 3.
+
+    /* Esto lo comento momentaneamente hasta tener claridad de como arreglarlo y defina la rutas de los modelos.
     if (process.env.NODE_ENV === 'development') {
       await sequelize.sync({ alter: true });
       console.log('🔄 Modelos sincronizados con la base de datos.');
     }
+    */
 
+    //Iniciamos el servidor
     app.listen(PORT, () => {
-      console.log(`🚀 Servidor FixIt corriendo en http://localhost:${PORT}`);
+      console.log(`🚀 Servidor LNE Stock corriendo en http://localhost:${PORT}`);
     });
   } catch (error) {
     console.error('❌ No se pudo iniciar el servidor:', error);
